99c3b387982d91e42b92523fdf7a066193274f2c
yyc777icanwritethem <yangyicheng7773@gmail.com>
2026-02-25 17:04:34 +0800

    Quick trunk-based change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 package hk.edu.hkbu.comp.comp4107.playground
-
+import hk.edu.hkbu.comp.comp4107.playground.Yicheng_Yang.SelfIntroduction
 import hk.edu.hkbu.comp.comp4107.playground.kennycheng.SelfIntro
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,11 +8,13 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    SelfIntro().introduce()
+    SelfIntroduction().selfIntroduction()
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+
+    println("hi")
 }
